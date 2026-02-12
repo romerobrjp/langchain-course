c1e975a443248b40384b397108f0106513acdef8
Romero Medeiros <romerobrjp@gmail.com>
2026-02-12 11:49:24 -0300

    initial version yet with react agent, before updating to tool calling (code from section 5)

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 from langchain.tools import tool
 from langchain_core.tools import render_text_description
-from langchain_core.prompts import PromptTemplate, ChatPromptTemplate
-from langchain_community.tools.tavily_search import TavilySearchResults
-from langgraph.prebuilt import create_react_agent
+from langchain_core.prompts import PromptTemplate
 from langchain_classic.agents.output_parsers.react_single_input import ReActSingleInputOutputParser
 from langchain_core.agents import AgentAction, AgentFinish
 from langchain_core.tools import BaseTool
@@ -13,24 +11,79 @@ from langchain_ollama import ChatOllama
 load_dotenv()
 
 @tool
-def multiply(x: float, y: float) -> float:
-    """Multiply 'x' times 'y'."""
-    return x * y
+def get_length_of_string(s: str)->int:
+    """Returns the length of the input string."""
+    print(f">> get_text_length enter with {s=}")
+    s = s.strip("'\n").strip('"')
+    return len(s)
+
+def find_tool_by_name(tools: Sequence[BaseTool], name: str) -> BaseTool:
+    for tool in tools:
+        if tool.name == name:
+            return tool
+    raise ValueError(f"Tool with name {name} not found")
+
+def main(input_str: str):
+    print(">> Hello from langchain-course-react!")
+    print(f">> Its length is: {get_length_of_string.invoke(input_str)}")
 
 if __name__ == "__main__":
-    print("Hello Tool Calling")
+    main('lion')
+    tools = [get_length_of_string]
+    print(f">> Available tools: {[tool.name for tool in tools]}")
+    template = """
+    Answer the following questions as best as you can. You have access to the following tools:
 
-    tools = [TavilySearchResults(), multiply]
+    {tools}
+
+    Use the following format:
+
+    Question: the input question you must answer
+    Thought: you should always think about what to do
+    Action: the action to take, should be one of [{tool_names}]
+    Action Input: the input to the action
+    Observation: the result of the action
+    ... (this Thought/Action/Action Input/Observation can repeat N times)
+    Thought: I now know the final answer
+    Final Answer: the final answer to the original input question
+
+    Begin!
+
+    Question: {input}
+    Thought: {agent_scratchpad}
+    """
+
+    prompt = PromptTemplate.from_template(template=template).partial(tools=render_text_description(tools), tool_names=", ".join([tool.name for tool in tools]))
+
     llm = ChatOllama(model="qwen2.5", temperature=0, stop=["\nObservation", "Observation", "Observation:"])
-    llm = ChatOllama(model="mistral", temperature=0)
+    agent = { 
+        "input": lambda x: x["input"],
+        "agent_scratchpad": lambda x: x.get("agent_scratchpad", "")
+    } | prompt | llm | ReActSingleInputOutputParser()
+    intermediate_steps = []
+    agent_step: Union[AgentAction, AgentFinish, None] = None
 
-    # Create agent using LangGraph
-    agent = create_react_agent(llm, tools)
+    while not isinstance(agent_step, AgentFinish):
+        agent_step = agent.invoke(
+            {
+                "input": "What is the length of the word 'lion'?",
+                "agent_scratchpad": intermediate_steps
+                # "agent_scratchpad": lambda x: format_log_to_str(x["agent_scratchpad"]) they use like this in the course but seems unnecessary
+            }
+        )
+        print(agent_step)
 
-    res = agent.invoke(
-        {
-            "messages": [("user", "what is the weather in dubai right now? compare it with San Fransisco, output should in in celsious")],
-        }
-    )
+        if isinstance(agent_step, AgentAction):
+            tool_name = agent_step.tool
+            tool_to_use = find_tool_by_name(tools, tool_name)
+            tool_input = agent_step.tool_input
+            tool_output = tool_to_use.invoke(tool_input)
 
-    print(res)
+            observation = tool_output
+            print(f">> Observation: {observation}")
+
+            print(f">> Tool {tool_name} output: {tool_output}")
+            intermediate_steps.append(f"\n\nThought: {agent_step.log}\nAction: {tool_name}\nAction Input: {tool_input}\nObservation: {observation}\n")
+
+    if isinstance(agent_step, AgentFinish):
+        print(f">> Final answer: {agent_step.return_values}")
