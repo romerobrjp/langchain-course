8e416ce1bbbb2fc966c552b055659ee2e1c5f208
Romero Medeiros <romerobrjp@gmail.com>
2026-02-23 14:47:50 -0300

    Enhance ingestion script by adding document loader and text splitter

--- ingestion.py
+++ ingestion.py
@@ -1,9 +1,17 @@
 import os
 from dotenv import load_dotenv
-
+from langchain_community.document_loaders import TextLoader
+from langchain_text_splitters import CharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
+from langchain_pinecone import PineconeVectorStore
 
 load_dotenv()
 
 if __name__ == '__main__':
     print("Ingesting...")
+    loader = TextLoader(
+        "C:\\Users\\romer\\repos\\langchain-course\\mediumblog1.txt", 
+        encoding="utf-8")
+    document = loader.load()
 
+    print("splitting...")
